464e7de35cdbcedc7c1fe4d86783e92eac26b3e4
Tam <21130168@st.hcmuaf.edu.vn>
2024-04-28 10:55:19 +0700

    add searchByGenre in mainjs and SearchController

--- src/main/java/Controller/SearchController.java
+++ src/main/java/Controller/SearchController.java
@@ -34,38 +34,76 @@ public class SearchController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String searchInput = request.getParameter("searchInput");
-		ArrayList<Song> songs = new DAOSong().selectAllBySearch(searchInput);
-		PrintWriter out = response.getWriter();
+		String numberput = request.getParameter("numberput");
+		int number = Integer.parseInt(numberput);
+		if(number == 1) {
+			String searchInput = request.getParameter("searchInput");
+			ArrayList<Song> songs = new DAOSong().selectAllBySearch(searchInput);
+			PrintWriter out = response.getWriter();
 
-		for (Song song : songs) {
-			out.println("<div class=\"item all-music-item\">\r\n"
-					+ "									<div class=\"inner-table\" id=\"" + song.getId_Song() + "\">\r\n"
-					+ "										<div class=\"inner-td\">\r\n"
-					+ "											<img src=\"" + song.getUrl_Img() + "\" width=75px height=75px\r\n"
-					+ "												alt=\"...\">\r\n"
-					+ "											<button type=\"button\" class=\"btn btn-music\" id=\"" + song.getId_Song() + "\"\r\n"
-					+ "												onclick=\"playMusic(this.id, '" + song.getName_Song() + "', '" + song.getSinger().getName_Singer() + "', '" + song.getUrl_Img() + "')\">\r\n"
-					+ "												<audio src=\"" + song.getUrl_Audio() + "\" class=\"au" + song.getId_Song() + "\"></audio>\r\n"
-					+ "												<i class=\"bi" + song.getId_Song() + " bi-play-circle\"></i>\r\n"
-					+ "											</button>\r\n"
-					+ "										</div>\r\n"
-					+ "										<div class=\"song-singer inner-td\">\r\n"
-					+ "											<b>" + song.getName_Song() + "</b> <br> <a href=\"\" class=\"singer\">" + song.getName_Song() + "\r\n"
-					+ "											</a>\r\n"
-					+ "										</div>\r\n"
-					+ "										<div class=\"inner-td\">\r\n"
-					+ "											<button class=\"btn btn-inner-td\">\r\n"
-					+ "												<i class=\"bi bi-share\" style=\"color: white; font-size: 13px\"></i>\r\n"
-					+ "											</button>\r\n"
-					+ "											<br>\r\n"
-					+ "											<button class=\"btn btn-inner-td\">\r\n"
-					+ "												<i class=\"bi bi-download\"\r\n"
-					+ "													style=\"color: white; font-size: 13px\"></i>\r\n"
-					+ "											</button>\r\n"
-					+ "										</div>\r\n" + "									</div>\r\n"
-					+ "								</div>");
+			for (Song song : songs) {
+				out.println("<div class=\"item all-music-item\">\r\n"
+						+ "									<div class=\"inner-table\" id=\"" + song.getId_Song() + "\">\r\n"
+						+ "										<div class=\"inner-td\">\r\n"
+						+ "											<img src=\"" + song.getUrl_Img() + "\" width=75px height=75px\r\n"
+						+ "												alt=\"...\">\r\n"
+						+ "											<button type=\"button\" class=\"btn btn-music\" id=\"" + song.getId_Song() + "\"\r\n"
+						+ "												onclick=\"playMusic(this.id, '" + song.getName_Song() + "', '" + song.getSinger().getName_Singer() + "', '" + song.getUrl_Img() + "')\">\r\n"
+						+ "												<audio src=\"" + song.getUrl_Audio() + "\" class=\"au" + song.getId_Song() + "\"></audio>\r\n"
+						+ "												<i class=\"bi" + song.getId_Song() + " bi-play-circle\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "										</div>\r\n"
+						+ "										<div class=\"song-singer inner-td\">\r\n"
+						+ "											<b>" + song.getName_Song() + "</b> <br> <a href=\"\" class=\"singer\">" + song.getName_Song() + "\r\n"
+						+ "											</a>\r\n"
+						+ "										</div>\r\n"
+						+ "										<div class=\"inner-td\">\r\n"
+						+ "											<button class=\"btn btn-inner-td\">\r\n"
+						+ "												<i class=\"bi bi-share\" style=\"color: white; font-size: 13px\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "											<br>\r\n"
+						+ "											<button class=\"btn btn-inner-td\">\r\n"
+						+ "												<i class=\"bi bi-download\"\r\n"
+						+ "													style=\"color: white; font-size: 13px\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "										</div>\r\n" + "									</div>\r\n"
+						+ "								</div>");
+			}
+		}else {
+			String genreInput = request.getParameter("searchInput2");
+			ArrayList<Song> song2 = new DAOSong().selectSongByGenre(genreInput);
+			PrintWriter out = response.getWriter();
+			for (Song song : song2) {
+				out.println("<div class=\"item all-music-item\">\r\n"
+						+ "									<div class=\"inner-table\" id=\"" + song.getId_Song() + "\">\r\n"
+						+ "										<div class=\"inner-td\">\r\n"
+						+ "											<img src=\"" + song.getUrl_Img() + "\" width=75px height=75px\r\n"
+						+ "												alt=\"...\">\r\n"
+						+ "											<button type=\"button\" class=\"btn btn-music\" id=\"" + song.getId_Song() + "\"\r\n"
+						+ "												onclick=\"playMusic(this.id, '" + song.getName_Song() + "', '" + song.getSinger().getName_Singer() + "', '" + song.getUrl_Img() + "')\">\r\n"
+						+ "												<audio src=\"" + song.getUrl_Audio() + "\" class=\"au" + song.getId_Song() + "\"></audio>\r\n"
+						+ "												<i class=\"bi" + song.getId_Song() + " bi-play-circle\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "										</div>\r\n"
+						+ "										<div class=\"song-singer inner-td\">\r\n"
+						+ "											<b>" + song.getName_Song() + "</b> <br> <a href=\"\" class=\"singer\">" + song.getName_Song() + "\r\n"
+						+ "											</a>\r\n"
+						+ "										</div>\r\n"
+						+ "										<div class=\"inner-td\">\r\n"
+						+ "											<button class=\"btn btn-inner-td\">\r\n"
+						+ "												<i class=\"bi bi-share\" style=\"color: white; font-size: 13px\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "											<br>\r\n"
+						+ "											<button class=\"btn btn-inner-td\">\r\n"
+						+ "												<i class=\"bi bi-download\"\r\n"
+						+ "													style=\"color: white; font-size: 13px\"></i>\r\n"
+						+ "											</button>\r\n"
+						+ "										</div>\r\n" + "									</div>\r\n"
+						+ "								</div>");
+			}
+		
 		}
+		
 	}
 
 	/**
